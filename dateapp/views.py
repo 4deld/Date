d86e891e1858a8142a71c267e30a29e74f5cc2e9
JaeHyun Kim <carbitna03@gmail.com>
2020-11-26 15:54:47 +0900

    json script
    nice kangsungwooKangSungWooKangSungWooKangSungWooKangSungWooKangSungWoo

--- dateapp/views.py
+++ dateapp/views.py
@@ -1,8 +1,11 @@
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from .models import Person, Info
+
 
 def index(request):
     a = Person.objects.get(username="Harry")
@@ -18,5 +21,25 @@ def detail(request, user_id):
 
 def calendar(request):
     informations=Info.objects.all().order_by('date') # 날짜순으로 정렬
-    print(informations)
-    return render(request, 'dateapp/calendar.html',{'informations':informations})
+    context={} #context- dict 내용물 - str
+    a=0
+    print(informations[1].user)
+    for row in informations.values_list():
+        # context.update({a:json.dumps(row,sort_keys=True,indent=4,cls=DjangoJSONEncoder)})
+        print(row)
+        InfoId=row[0]
+        personname=str(informations[a].user)
+        date=str(row[2])
+        time=str(row[3])
+        do=str(row[4])
+        #[infotable의 id, user_id, date, time, do]
+        x={
+            "InfoId":InfoId,"username": personname,"date":date,"time":time,"do":do,
+        }
+        y= json.dumps(x,sort_keys=False,cls=DjangoJSONEncoder)
+        print(y)
+        context.update({a:y})
+        a=a+1
+
+    print(context)
+    return render(request, 'dateapp/calendar.html',{'context':context})
